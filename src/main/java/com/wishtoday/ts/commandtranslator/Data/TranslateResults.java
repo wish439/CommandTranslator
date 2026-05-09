407833960db1c23d1e5919ce1975b5ed10506ba7
wishtoday238 <wishtoday238@gmail.com>
2026-05-10 00:42:03 +0800

    emmmmm

--- src/main/java/com/wishtoday/ts/commandtranslator/Data/TranslateResults.java
+++ src/main/java/com/wishtoday/ts/commandtranslator/Data/TranslateResults.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 
 public class TranslateResults<T> {
     @Getter
-    private T result;
+    private final T result;
     @Getter
-    private StringRange range;
+    private final StringRange range;
     public TranslateResults(T result, StringRange range) {
         this.result = result;
         this.range = range;
